cb94fc3f0cf4f2718f12f0a79d34db4dd2fa794e
Kang-yeonbae <dusqo7951@hurdlers.kr>
2025-08-18 14:39:42 +0900

    버쉘 프쉬오류해결중

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    rules: {
+      // TypeScript 관련 규칙 비활성화
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    ignores: ["**/*.ts", "**/*.tsx"], // TypeScript 파일 무시
+  },
+];
 
 export default eslintConfig;
